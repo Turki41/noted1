3a9d48aa891dd20dd34827ad9681fb6b35485f03
Turki <turkisalh97@gmail.com>
2025-10-29 13:20:17 +0300

    path update

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://noted1.com/api',
+    baseURL: import.meta.env.VITE_BASE_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
